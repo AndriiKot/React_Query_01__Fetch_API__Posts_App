1a13ea01a78634751cd3f25c4755e52114021984
AndriiKot <a_kocyuba@ukr.net>
2024-06-27 00:33:18 +0200

    React 'React Query 04' ! react qyery testing fetch start 1 !

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import Loading from "./Loading/Loading";
 import FetchPosts from "./FetchPosts/FetchPosts";
 
 function App() {
-  const [posts, loading, error] = FetchPosts();
+  const { loading, error, posts } = FetchPosts();
+  console.log(posts);
+  // if (loading) {
+  //   return <Loading />;
+  // }
+  // if (error) {
+  //   return <p>Error: {error.message}</p>;
+  // }
 
-  if (loading) {
-    return <Loading />;
-  }
-  if (error) {
-    return <p>Error: {error.message}</p>;
-  }
-
-  if (posts.length == 0) {
-    return <p>No posts found.</p>;
-  }
+  // if (posts.length == 0) {
+  //   return <p>No posts found.</p>;
+  // }
 
   return <CardsList list={posts} />;
 }
--- src/FetchPosts/FetchPosts.jsx
+++ src/FetchPosts/FetchPosts.jsx
@@ -1,32 +1,10 @@
-﻿import { useEffect, useState } from "react";
+﻿const fetchPosts = async () => {
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts").then(
+    (response) => response.json()
+  );
+  return data;
+};
 
-export default function FetchPosts() {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+fetchPosts();
 
-  const fetchPosts = async () => {
-    try {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      if (!response.ok) {
-        throw new Error("Failed to fetch posts");
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-      setError(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchPosts()
-      .then((data) => setPosts(data))
-      .then(() => setLoading(false));
-  }, []);
-  
-
-  return [posts, loading, error];
-}
+export default fetchPosts;
