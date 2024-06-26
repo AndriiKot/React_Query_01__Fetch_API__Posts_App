1e374b518a7793c1071d45da2cf9f968dd66cfaf
AndriiKot <a_kocyuba@ukr.net>
2024-06-27 00:13:44 +0200

    React 'React Query 04' ! install  react-query  !

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { QueryClient } from 'https://esm.sh/@tanstack/react-query'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
